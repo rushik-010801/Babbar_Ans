12e163053e483464312559d93ae9e5ed8837e64c
rushik-010801 <52701863+rushik-010801@users.noreply.github.com>
2021-04-21 23:09:05 +0530

    Create Negative_elements_One_side.java
    An array contains both positive and negative numbers in random order. Rearrange the array elements so that all negative numbers appear before all positive numbers.
    
    There are two approaches :
    1. Partition process of quick sort.
    2. Two pointer approach (Above).

---
+++ Arrays/Negative_elements_One_side.java
@@ -0,0 +1,36 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.Scanner;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr= new int[n];
+		for(int i = 0; i < n; i++){
+		    arr[i] = sc.nextInt();
+		}
+		int left = 0, right = n-1;
+		while(left <= right){
+		    if(arr[left] < 0 && arr[right] < 0)
+		        left++;
+		    else if(arr[left] > 0 && arr[right] > 0)
+		        right--;
+		    else if(arr[left] > 0 && arr[right] < 0){
+		        int temp = arr[left];
+		        arr[left] = arr[right];
+		        arr[right] = temp;
+		        left++;
+		        right--;
+		    }
+		    else{
+		        left++;
+		        right--;
+		    }
+		}
+		for(int i = 0; i < arr.length; i++){
+		    System.out.print(arr[i] + " ");
+		}
+	}
+}
