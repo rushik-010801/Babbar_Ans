2decbf3f92ec99dcb790b9bfd94cdcbbd175ca3e
rushik-010801 <52701863+rushik-010801@users.noreply.github.com>
2021-04-21 22:49:04 +0530

    Create Sorting_0_1_2.java
    Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.

---
+++ Arrays/Sorting_0_1_2.java
@@ -0,0 +1,47 @@
+//https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1#
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        // My appraoch
+        /*int z = 0, o = 0, t = 0;
+        for(int i = 0; i < a.length; i++){
+            if(a[i] == 0)
+                z++;
+            else if(a[i] == 1)
+                o++;
+            else
+                t++;
+        }
+        int i = 0;
+        int temp = i;
+        for(; i < a.length && i < temp + z; i++){
+            a[i] = 0;
+        }
+        temp = i;
+        for(; i < a.length && i < temp + o; i++){
+            a[i] = 1;
+        }
+        temp = i;
+        for(; i < a.length && i < temp + t; i++){
+            a[i] = 2;
+        }*/
+        int low = 0, high = n-1, mid = 0;
+        while(mid <= high){
+            if(a[mid]==0) {
+                int temp=a[low];
+                a[low]=a[mid];
+                a[mid]=temp;
+                low++;
+                mid++;
+            } else if(a[mid]==1) {
+                mid++;
+            } else {
+                int temp=a[mid];
+                a[mid]=a[high];
+                a[high]=temp;
+                high--;
+            }
+        }
+    }
+}
